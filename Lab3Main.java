3df2ab243506544a9a341cedf70e307d235a8802
Karthik N G <karthiknaduthota28@gmail.com>
2025-09-02 22:17:25 +0530

    Update and rename 3Main.java to Lab3Main.java

--- Lab3Main.java
+++ Lab3Main.java
@@ -3,10 +3,10 @@ import Transport.*;
 public class Main {
 		public static void main(String[] args) {
 			Ebike e = new Ebike("EB-101", 50);
-	        e.deliver("Sandwich", "Library");
+	        e.deliver("Burger", "Library");
 	        Drone d = new Drone("DR-1");
 	        d.deliver("Notes", "ExamCell");
-	        d.deliver("USB", "CSE Block");
+	        d.deliver("USB", "ISE Block");
 	        double bill = d.cost(5);
 	        System.out.println("Drone delivery cost: Rs." + bill);
 
@@ -15,3 +15,4 @@ public class Main {
 	}
 
 }
+
